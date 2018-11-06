16d31e0fa0a27cb0ed8e73f9c260a254fe4f7df3
me <me@example.com>
2018-11-06 15:19:33 +0100

    V0.5 released

--- FindCryptoPercentages.py
+++ FindCryptoPercentages.py
@@ -38,49 +38,43 @@ percentageOfMaxSupply=0
 #total_value=0
 
 # Used for clearer understanding of indeces in lists
-CONST_COINS_NUMBER=0
-CONST_PRICE       =1
-CONST_PRC_24H_CHANGE =2
-CONST_PURCH_PRICE =3
-CONST_PRC_PURCH_PRICE=4
-CONST_TOTAL_VALUE =5
-CONST_CIRC_SUPPLY =6
+CONST_COINS_NUMBER   =0
+CONST_PURCH_PRICE    =1
+CONST_PRICE          =2
+CONST_PRC_PURCH_PRICE=3
+CONST_PRC_24H_CHANGE =4
+CONST_TOTAL_VALUE    =5
+CONST_CIRC_SUPPLY    =6
 CONST_PRC_CIRC_SUPPLY=7
-CONST_TOT_SUPPLY  =8
+CONST_TOT_SUPPLY     =8
 CONST_PRC_TOT_SUPPLY =9
-CONST_MAX_SUPPLY  =10
+CONST_MAX_SUPPLY     =10
 CONST_PRC_MAX_SUPPLY =11
 
 
 
-keys_to_sort=["price","prc24h","purchaseprice","prcpurchase","totalvalue","circsuppply","prccircsupply","totalsupply","prctotalsupply","maxsupply","prcmaxsupply"]
-attributes_to_print=[]
+keys_to_sort=["purchaseprice","price","prcpurchase","prc24h","totalvalue","circsuppply","prccircsupply","totalsupply","prctotalsupply","maxsupply","prcmaxsupply"]
+attributes_to_print=[]    # A list of the attributes of each coin to be printed so that the report fits in the console
 
-
-purchase_price={}
-marked_coins=[]
-#global unmarked_counter                # Holds the number of coins not marked. Used only if all coins are not marked
-#unmarked_counter=0
-#global no_coins_purchase_price         # Holds the number of coins that have a purchase price. Used only if all coins are not marked
-#no_coins_purchase_price=0
+marked_coins=[]    # A list of the coins to be marked with Magenta
 
 # Coin name must be exactly as it is in coinmarketcap page: https://coinmarketcap.com
 coins_dict = {
-    'Ethereum': 50,
-    'Bitcoin': 0.5,
-    'Aeternity': 1800,
-    'Stellar': 8800,
-    'PIVX': 1450,
-    'EOS': 249.8,
-    'Dash': 7.5,
-    'Lisk': 390,
-    'Tezos': 600,
-    'Basic Attention Token': 2592.1,
-    'Waves': 211.3,
-    'Ethereum Classic': 43.8,
-    'SONM': 6300,
-    'Particl': 155,
-    'TaaS': 400,
+    'Ethereum': [50,0.2],
+    'Bitcoin': [0.5,0],
+    'Aeternity': [1800,0.3],
+    'Stellar': [8800,0.15],
+    'PIVX': [1450,0.4],
+    'EOS': [249.8,0],
+    'Dash': [7.5,90],
+    'Lisk': [390,0.6],
+    'Tezos': [600,0.6],
+    'Basic Attention Token': [2592.1,0.25],
+    'Waves': [211.3,0.4],
+    'Ethereum Classic': [43.8,0],
+    'SONM': [6300,0],
+    'Particl': [155,0],
+    'TaaS': [400,1],
     }
 
 def myconsoleprint(text1,text2="",text3=""):
@@ -89,8 +83,9 @@ def myconsoleprint(text1,text2="",text3=""):
 
 def initialize(dictofcoins):
    for coin in dictofcoins:
-      dictofcoins[coin]=[dictofcoins[coin]]
-      dictofcoins[coin].extend(['price','prc24h','purchaseprice','prcpurchase','totalvalue','circsuppply','prccircsupply','totalsupply','prctotalsupply','maxsupply','prcmaxsupply'])
+#      dictofcoins[coin]=[dictofcoins[coin]]
+      dictofcoins[coin].extend(['price','prcpurchase','prc24h','totalvalue','circsuppply','prccircsupply','totalsupply','prctotalsupply','maxsupply','prcmaxsupply'])
+   myconsoleprint(dictofcoins)
 
 def sort_based_on(dictionary,index):
    list_to_sort=sorted(dictionary.items(), key=lambda e: e[1][index])          # Sorted return a list of Tupples
@@ -148,114 +143,66 @@ def print_attributes(attr,list_of_coins):
        print(Style.RESET_ALL)
        total_value=total_value+sorted_list[coin][5]
 
-def handle_base_mark_and_purchase_price(dictionary_with_coins):
-   global unmarked_counter
-   unmarked_counter = 0
-   global no_coins_purchase_price
-   no_coins_purchase_price=0
+def handle_base_and_mark(dictionary_with_coins):
    global base_coin
    base_coin=""
    base_is_set=0
 
    for coin in dictionary_with_coins:
       print(coin,": ", end="")
-      purchase_price[coin] = input("")
-      if "end" in purchase_price[coin]:
-         del purchase_price[coin]
+      action = input("")
+      if action=="end":
+         myconsoleprint("action is \"end\"")
+         myconsoleprint(marked_coins)
          break
-      templist=purchase_price[coin].split(",")
-      templist=[value.lower() for value in templist]    # .lower() works since purchase price is considered a string here
-      if "" in templist:
-         myconsoleprint("\"\" in templist")
-         unmarked_counter = unmarked_counter + 1
-         no_coins_purchase_price = no_coins_purchase_price + 1
-         templist[0]=0
-         purchase_price[coin] = templist
-         myconsoleprint(unmarked_counter,no_coins_purchase_price)
-         myconsoleprint(purchase_price[coin])
-      elif "mark" in templist and len(templist) == 1:
-         myconsoleprint("\"mark\" in templist and len(templist)==1")
-         marked_coins.append(coin)
-         templist.remove("mark")
-         templist.append("0")
-         purchase_price[coin] = templist
-         no_coins_purchase_price = no_coins_purchase_price + 1
+      if action=="":
+         myconsoleprint("action is \"\"")
          myconsoleprint(marked_coins)
-         myconsoleprint(unmarked_counter,no_coins_purchase_price)
-         myconsoleprint(purchase_price[coin])
-      elif "mark" in templist and "base" in templist:
-         print("Cannot have both base and mark")
-      elif "mark" in templist and "base" not in templist and len(templist) == 2:
-         myconsoleprint("\"mark\" in templist and len(templist)==2")
+      elif action=="mark":
+         myconsoleprint("action is \"mark\"")
          marked_coins.append(coin)
-         templist.remove("mark")
-         purchase_price[coin] = templist
          myconsoleprint(marked_coins)
-         myconsoleprint(unmarked_counter,no_coins_purchase_price)
-         myconsoleprint(purchase_price[coin])
-      elif "base" in templist and len(templist) == 1:
-         myconsoleprint("\"base\" in templist and len(templist)==1")
+      elif action=="base":
+         myconsoleprint("action is \"base\"")
          if base_is_set==1:
              print("Error: You can only have one base coin")
              break
          base_coin=coin
          base_is_set=1
-         templist.remove("base")
-         templist.append("0")
-         purchase_price[coin] = templist
-         no_coins_purchase_price = no_coins_purchase_price + 1
          myconsoleprint("Base coin: ", base_coin)
          myconsoleprint(marked_coins)
-         myconsoleprint(unmarked_counter,no_coins_purchase_price)
-         myconsoleprint(purchase_price[coin])
-      elif "base" in templist and len(templist) == 2:
-         myconsoleprint("\"base\" in templist and len(templist)==2")
-         if base_is_set==1:
-             print("Error: You can only have one base coin")
-             break
-         base_coin=coin
-         base_is_set=1
-         templist.remove("base")
-         purchase_price[coin] = templist
-         myconsoleprint("Base coin: ", base_coin)
-         myconsoleprint(marked_coins)
-         myconsoleprint(unmarked_counter,no_coins_purchase_price)
-         myconsoleprint(purchase_price[coin])
-      elif len(templist) == 1:
-         myconsoleprint("len(templist) == 1")
-         unmarked_counter = unmarked_counter + 1
-         purchase_price[coin] = templist
-         myconsoleprint(unmarked_counter,no_coins_purchase_price)
-         myconsoleprint(purchase_price[coin])
 
    myconsoleprint(marked_coins)
-   myconsoleprint(purchase_price)
-   print("Are all the settings correct?")
-   
-print("For each coin declare if you want to mark it with green color by writing \"mark\" comma the purchase price. The order is not important")
-print("You do not have to use either \"mark\" or give a purchase price")
-print("You can skip any coin configuration with the enter key")
+   check=input("Are all the settings correct?")
+
+def get_user_input(list_of_keys_to_sort):
+   global key_to_sort
+   global attributes_to_print
+   for key in list_of_keys_to_sort:
+      print(list_of_keys_to_sort.index(key)+1,")",key)
+
+   print("-"*25)
+   key_to_sort = input("Please choose the key to use for descending sorting: ")   # This returns string in Python 3
+   key_to_sort = int(key_to_sort)                                                 # This is the index of the key in the list "keys_to_sort"
+
+   attributes_to_print = input("Enter attributes to print. Example \"1,5,8\": ")
+   attributes_to_print = attributes_to_print.split(",")
+   attributes_to_print = set(attributes_to_print)        # Unordered list with unique elements
+   attributes_to_print = list(attributes_to_print)
+   attributes_to_print.sort()
+   for i in range(0,len(attributes_to_print)):
+         attributes_to_print[i]=int(attributes_to_print[i])
+   myconsoleprint(attributes_to_print)
+
+
+print("For each coin declare if you want to mark it with Magenta color by writing \"mark\"")
+print("For one coin only declare if you want it as base coin by writing \"base\". Base is used as separator in the report when you use sorting")
+print("You do not have to use either \"mark\" or \"base\". You can skip any coin configuration with the enter key")
 print("You can end the setup with \"end\"")
-handle_base_mark_and_purchase_price(coins_dict)
 
 initialize(coins_dict)
-
-# Ask user which is the key to sort
-for key in keys_to_sort:
-   print(keys_to_sort.index(key)+1,")",key)
-
-print("-"*25)
-key_to_sort = input("Please choose the key to use for descending sorting: ")   # This returns string in Python 3
-key_to_sort = int(key_to_sort)                                                 # This is the index of the key in the list "keys_to_sort"
-
-attributes_to_print = input("Enter attributes to print. Example \"1,5,8\": ")
-attributes_to_print = attributes_to_print.split(",")
-attributes_to_print = set(attributes_to_print)        # Unordered list with unique elements
-attributes_to_print = list(attributes_to_print)
-attributes_to_print.sort()
-for i in range(0,len(attributes_to_print)):
-      attributes_to_print[i]=int(attributes_to_print[i])-1
-myconsoleprint(attributes_to_print)
+handle_base_and_mark(coins_dict)
+get_user_input(keys_to_sort)
 
 print("\n"*1)
 print("Reading...")
@@ -271,7 +218,7 @@ for coin in coins_dict:
    coinId=get_coinmarketcap_ID(coin)
    
    CoinDataCoinmarketcap="https://api.coinmarketcap.com/v2/ticker/"+str(coinId)+"/?convert=EUR"
-   # Turn the string returned from urllib.request.urlopen to python dictionary with .loads method
+# Turn the string returned from urllib.request.urlopen to python dictionary with .loads method
    coinData=json.loads(urllib.request.urlopen(CoinDataCoinmarketcap).read())
 
    circSupply=coinData['data']['circulating_supply']
@@ -280,7 +227,7 @@ for coin in coins_dict:
 
    price=format(coinData['data']['quotes']['EUR']['price'],'.3f')
    price_change_24h=float(coinData['data']['quotes']['EUR']['percent_change_24h'])
-   initial_purchased_price=format(float(purchase_price[coin][0]),'.3f')
+   initial_purchased_price=format(float(coins_dict[coin][CONST_PURCH_PRICE]),'.3f')
 
    coins_dict[coin][CONST_PRICE]          =float(price)                                                          # "Price"
    coins_dict[coin][CONST_PRC_24H_CHANGE] =float(price_change_24h)/100                                           # "Prc24h"
@@ -313,13 +260,9 @@ for coin in coins_dict:
       coins_dict[coin][CONST_MAX_SUPPLY]    =0
       coins_dict[coin][CONST_PRC_MAX_SUPPLY]=0
 
-myconsoleprint(coins_dict)
+# myconsoleprint(coins_dict)
 sorted_list=sort_based_on(coins_dict,key_to_sort)
-myconsoleprint(sorted_list)
-#"Name","Coins","Price",  "%24h",  "Value","PPrice", "%PPrice","Circ supply","%Cir supply","Tot supply","%Tot supply","Max supply","%Max supply"
-#{:<21}| {:^7}| {:^9}|   {:^4}|    {:^8}|  {:^9}|     {:^4}|     {:^14}|       {:^11}|      {:^15}|       {:^11}|      {:^15}|        {:^11}
-#{:<21}| {:>7}|{:>9,.3f}|{:^4.1%}| {:>8,}| {:^9,.3f}| {:^4.1%}| {:>14,.0f}|  {:>11.8%}|    {:>15,.0f}|   {:>11.8%}|    {:>15,.0f}|   {:>11.8%}
-
+# myconsoleprint(sorted_list)
 print_attributes(attributes_to_print,sorted_list)
 
 print("-"*145)
